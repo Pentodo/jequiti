6d9eb03083acf15ec09af42f2b05374ab3fbd29f
Matheus Alberto <matheus.a.v.b@gmail.com>
2022-07-27 00:53:57 -0300

    new data

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,5 +1,8 @@
 export const gameData = {
-	comida: ['arroz', 'feijão', 'carne'],
-	carro: ['fusca', 'uno', 'brasília'],
-	CEP: ['salvador', 'bahia', 'brasil', 'londres', 'uruguai'],
+	comida: ['batata', 'couve', 'sushi', 'pizza', 'lasanha'],
+	fruta: ['jabuticaba', 'morango', 'banana', 'lichia', 'carambola', 'abacaxi', 'uva'],
+	animal: ['toupeira', 'canguru', 'gato', 'onça', 'cavalo', 'esquilo', 'panda'],
+	carro: ['fusca', 'uno', 'gol', 'corsa', 'fiorino', 'palio'],
+	roupa: ['saia', 'blusa', 'camisa', 'sapato', 'meia', 'touca', 'cueca', 'calcinha'],
+	jogo: ['valorant', 'lol', 'uno', 'xadrez', 'mario', 'habbo', 'transformice', 'terraria', 'minecraft'],
 };
